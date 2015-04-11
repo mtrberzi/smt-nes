8bb81a2cb84e51490ca385ddbfd09a8992864fc9
Murphy Berzish <murphy.berzish@gmail.com>
2015-04-11 16:08:16 -0400

    add indexed identifiers

--- src/main/java/io/lp0onfire/smtnes/smt2/IndexedIdentifier.java
+++ src/main/java/io/lp0onfire/smtnes/smt2/IndexedIdentifier.java
@@ -1,5 +1,9 @@
 package io.lp0onfire.smtnes.smt2;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
 import org.apache.commons.lang3.ArrayUtils;
 
 public class IndexedIdentifier extends ExpressionList {
@@ -7,6 +11,9 @@ public class IndexedIdentifier extends ExpressionList {
   // Indexed identifiers are defined as the application
   // of the reserved word _ to a symbol
   // and one or more indices, given by numerals.
+  
+  private final Symbol symbol;
+  private final List<Numeral> indices;
   
   public IndexedIdentifier(Symbol sym, Numeral... numerals) {
     super(ArrayUtils.addAll(new SExpression[]{
@@ -16,7 +23,8 @@ public class IndexedIdentifier extends ExpressionList {
     if (numerals.length == 0) {
       throw new IllegalArgumentException("indexed identifier must contain at least one index");
     } else {
-      
+      this.symbol = sym;
+      this.indices = new LinkedList<Numeral>(Arrays.asList(numerals));
     }
   }
   
