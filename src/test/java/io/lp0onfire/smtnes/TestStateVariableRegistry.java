e08c3e6dd9ec5ee19a8c71d2cf92f4e70eba2cee
Murphy Berzish <murphy.berzish@gmail.com>
2015-04-20 15:29:11 -0400

    complete unit tests for state variable registry

--- src/test/java/io/lp0onfire/smtnes/TestStateVariableRegistry.java
+++ src/test/java/io/lp0onfire/smtnes/TestStateVariableRegistry.java
@@ -45,4 +45,86 @@ public class TestStateVariableRegistry {
     reg.apply(gen);
   }
   
+  @Test(expected=IllegalStateException.class)
+  public void testReadBeforeWrite_Illegal() {
+    StateVariableRegistry reg = new StateVariableRegistry();
+    CodeGenerator gen  = new CodeGenerator(){
+      @Override
+      public List<String> getStateVariablesRead() {
+        return Arrays.asList("foo");
+      }
+
+      @Override
+      public List<String> getStateVariablesWritten() {
+        return new LinkedList<>();
+      }
+
+      @Override
+      public List<SExpression> generateCode(Map<String, IndexedIdentifier> inputs,
+          Map<String, IndexedIdentifier> outputs) {
+        return new LinkedList<>();
+      }
+    };
+    reg.apply(gen);
+  }
+  
+  @Test
+  public void testReadAfterWrite() {
+    StateVariableRegistry reg = new StateVariableRegistry();
+    CodeGenerator init = new CodeGenerator() {
+
+      @Override
+      public List<String> getStateVariablesRead() {
+        return new LinkedList<>();
+      }
+
+      @Override
+      public List<String> getStateVariablesWritten() {
+        return Arrays.asList("foo");
+      }
+
+      @Override
+      public List<SExpression> generateCode(
+          Map<String, IndexedIdentifier> inputs,
+          Map<String, IndexedIdentifier> outputs) {
+        return new LinkedList<>();
+      }
+      
+    };
+    CodeGenerator update = new CodeGenerator() {
+
+      @Override
+      public List<String> getStateVariablesRead() {
+        return Arrays.asList("foo");
+      }
+
+      @Override
+      public List<String> getStateVariablesWritten() {
+        return Arrays.asList("foo");
+      }
+
+      @Override
+      public List<SExpression> generateCode(
+          Map<String, IndexedIdentifier> inputs,
+          Map<String, IndexedIdentifier> outputs) {
+        // we should see input: (_ foo 0) and output: (_ foo 1)
+        
+        assertTrue(inputs.containsKey("foo"));
+        IndexedIdentifier foo_in = inputs.get("foo");
+        assertEquals("input 'foo' has wrong symbol name", "foo", foo_in.getSymbol().getName());
+        assertEquals("input 'foo' has wrong index", "0", foo_in.getIndices().get(0).getDigits());
+        
+        assertTrue(outputs.containsKey("foo"));
+        IndexedIdentifier foo_out = outputs.get("foo");
+        assertEquals("output 'foo' has wrong symbol name", "foo", foo_out.getSymbol().getName());
+        assertEquals("output 'foo' has wrong index", "1", foo_out.getIndices().get(0).getDigits());
+        
+        return new LinkedList<>();
+      }
+      
+    };
+    reg.apply(init);
+    reg.apply(update);
+  }
+  
 }
