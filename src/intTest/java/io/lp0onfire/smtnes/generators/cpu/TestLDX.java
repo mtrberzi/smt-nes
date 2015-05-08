a238d04ba3ff806b566f724c4c824a05259de28a
Murphy Berzish <murphy.berzish@gmail.com>
2015-05-07 22:26:40 -0400

    tests for LDX (based on LDA tests)

---
+++ src/intTest/java/io/lp0onfire/smtnes/generators/cpu/TestLDX.java
@@ -0,0 +1,617 @@
+package io.lp0onfire.smtnes.generators.cpu;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+import io.lp0onfire.smtnes.CodeGenerator;
+import io.lp0onfire.smtnes.PageHandler;
+import io.lp0onfire.smtnes.StateVariableRegistry;
+import io.lp0onfire.smtnes.Z3;
+import io.lp0onfire.smtnes.smt2.Assertion;
+import io.lp0onfire.smtnes.smt2.BitVectorDeclaration;
+import io.lp0onfire.smtnes.smt2.EqualsExpression;
+import io.lp0onfire.smtnes.smt2.HexConstant;
+import io.lp0onfire.smtnes.smt2.Numeral;
+import io.lp0onfire.smtnes.smt2.SExpression;
+import io.lp0onfire.smtnes.smt2.Symbol;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.junit.Test;
+
+public class TestLDX {
+  
+  @Test(timeout=60 * 1000)
+  public void testLDX_IMM() throws IOException {
+    // opcode A2
+    
+    // run the program
+    // $0000: A2 5A  LDX #$5A
+    // and expect to see $5A in X
+    
+    List<SExpression> exprs = new LinkedList<>();
+    StateVariableRegistry reg = new StateVariableRegistry();
+    
+    ROMBurner burner = new ROMBurner("TestROM");
+    burner.write(0x000, 0xA2);
+    burner.write(0x001, 0x5A);
+    // reset vector
+    burner.write(0xFFD, 0x00); // high byte
+    burner.write(0xFFC, 0x00); // low byte
+    
+    
+    // map the first and last page into RAM
+    ArrayList<PageHandler> pageHandlers = new ArrayList<>(16);
+    PageHandler ramPageHandler = burner.getCPUPageHandler();
+    PageHandler nullPageHandler = new NullPageHandler();
+    pageHandlers.add(0, ramPageHandler);
+    for (int i = 1; i < 15; ++i) {
+      pageHandlers.add(i, nullPageHandler);
+    }
+    pageHandlers.add(15, ramPageHandler);
+    
+    CodeGenerator memoryControllerFront = new CPUMemoryControllerFrontHalf(pageHandlers);
+    CodeGenerator memoryControllerBack = new CPUMemoryControllerBackHalf(pageHandlers);
+    
+    // initialize CPU once
+    CodeGenerator cpuPowerOn = new CPUPowerOn();
+    exprs.addAll(reg.apply(cpuPowerOn));
+    
+    // initialize RAM
+    CodeGenerator initRAM = burner.getInitializer();
+    exprs.addAll(reg.apply(initRAM));
+    
+    CodeGenerator cpuCycle = new CPUCycle();
+    CodeGenerator verifyStateInstructionFetch = new VerifyCPUState(CPUState.InstructionFetch);
+    
+    // reset sequence
+    for (int i = 0; i < 8; ++i) {
+      exprs.addAll(reg.apply(cpuCycle));
+      exprs.addAll(reg.apply(memoryControllerFront));
+      exprs.addAll(reg.apply(nullPageHandler));
+      exprs.addAll(reg.apply(ramPageHandler));
+      exprs.addAll(reg.apply(memoryControllerBack));
+    }
+    exprs.addAll(reg.apply(verifyStateInstructionFetch));
+    
+    // execute instruction
+    // LDA immediate takes 2 cycles
+    
+    for (int i = 0; i < 2; ++i) {
+      exprs.addAll(reg.apply(cpuCycle));
+      exprs.addAll(reg.apply(memoryControllerFront));
+      exprs.addAll(reg.apply(nullPageHandler));
+      exprs.addAll(reg.apply(ramPageHandler));
+      exprs.addAll(reg.apply(memoryControllerBack));
+    }
+    
+    // check that X = $5A
+    CodeGenerator verifyX = new CodeGenerator() {
+
+      @Override
+      public Set<String> getStateVariablesRead() {
+        return new HashSet<String>(Arrays.asList(new String[]{
+            "CPU_X"
+        }));
+      }
+
+      @Override
+      public Set<String> getStateVariablesWritten() {
+        return new HashSet<>();
+      }
+
+      @Override
+      public List<SExpression> generateCode(Map<String, Symbol> inputs,
+          Map<String, Symbol> outputs) {
+        List<SExpression> exprs = new LinkedList<>();
+        
+        Symbol X = inputs.get("CPU_X");
+        exprs.add(new Assertion(new EqualsExpression(X, new HexConstant("5A"))));
+        
+        return exprs;
+      }
+      
+    };
+    exprs.addAll(reg.apply(verifyX));
+    
+    
+    try(Z3 z3 = new Z3()) {
+      z3.open();
+      for(SExpression expr : exprs) {
+        z3.write(expr.toString());
+      }
+      assertTrue(z3.checkSat());
+    }
+    
+  }
+  
+  @Test
+  public void testLDX_ZPG() throws IOException {
+    // opcode A6
+    
+    // run the program
+    // $0000: A6 10  LDA $10
+    // ...
+    // $0010: 2A
+    // and expect to see $2A in register X
+    
+    List<SExpression> exprs = new LinkedList<>();
+    StateVariableRegistry reg = new StateVariableRegistry();
+    
+    ROMBurner burner = new ROMBurner("TestROM");
+    burner.write(0x000, 0xA6);
+    burner.write(0x001, 0x10);
+    burner.write(0x010, 0x2A);
+    // reset vector
+    burner.write(0xFFD, 0x00); // high byte
+    burner.write(0xFFC, 0x00); // low byte
+    
+    
+    // map the first and last page into RAM
+    ArrayList<PageHandler> pageHandlers = new ArrayList<>(16);
+    PageHandler ramPageHandler = burner.getCPUPageHandler();
+    PageHandler nullPageHandler = new NullPageHandler();
+    pageHandlers.add(0, ramPageHandler);
+    for (int i = 1; i < 15; ++i) {
+      pageHandlers.add(i, nullPageHandler);
+    }
+    pageHandlers.add(15, ramPageHandler);
+    
+    CodeGenerator memoryControllerFront = new CPUMemoryControllerFrontHalf(pageHandlers);
+    CodeGenerator memoryControllerBack = new CPUMemoryControllerBackHalf(pageHandlers);
+    
+    // initialize CPU once
+    CodeGenerator cpuPowerOn = new CPUPowerOn();
+    exprs.addAll(reg.apply(cpuPowerOn));
+    
+    // initialize RAM
+    CodeGenerator initRAM = burner.getInitializer();
+    exprs.addAll(reg.apply(initRAM));
+    
+    CodeGenerator cpuCycle = new CPUCycle();
+    CodeGenerator verifyStateInstructionFetch = new VerifyCPUState(CPUState.InstructionFetch);
+    
+    // reset sequence
+    for (int i = 0; i < 8; ++i) {
+      exprs.addAll(reg.apply(cpuCycle));
+      exprs.addAll(reg.apply(memoryControllerFront));
+      exprs.addAll(reg.apply(nullPageHandler));
+      exprs.addAll(reg.apply(ramPageHandler));
+      exprs.addAll(reg.apply(memoryControllerBack));
+    }
+    exprs.addAll(reg.apply(verifyStateInstructionFetch));
+    
+    // execute instruction
+    // LDA zero-page takes 3 cycles
+    
+    for (int i = 0; i < 3; ++i) {
+      exprs.addAll(reg.apply(cpuCycle));
+      exprs.addAll(reg.apply(memoryControllerFront));
+      exprs.addAll(reg.apply(nullPageHandler));
+      exprs.addAll(reg.apply(ramPageHandler));
+      exprs.addAll(reg.apply(memoryControllerBack));
+    }
+    
+    // check that X = $2A
+    CodeGenerator verifyX = new CodeGenerator() {
+
+      @Override
+      public Set<String> getStateVariablesRead() {
+        return new HashSet<String>(Arrays.asList(new String[]{
+            "CPU_X"
+        }));
+      }
+
+      @Override
+      public Set<String> getStateVariablesWritten() {
+        return new HashSet<>();
+      }
+
+      @Override
+      public List<SExpression> generateCode(Map<String, Symbol> inputs,
+          Map<String, Symbol> outputs) {
+        List<SExpression> exprs = new LinkedList<>();
+        
+        Symbol X = inputs.get("CPU_X");
+        exprs.add(new Assertion(new EqualsExpression(X, new HexConstant("2A"))));
+        
+        return exprs;
+      }
+      
+    };
+    exprs.addAll(reg.apply(verifyX));
+    
+    
+    try(Z3 z3 = new Z3()) {
+      z3.open();
+      for(SExpression expr : exprs) {
+        z3.write(expr.toString());
+      }
+      assertTrue(z3.checkSat());
+    }
+    
+  }
+  
+  @Test
+  public void testLDX_ZPY() throws IOException {
+    // opcode B6
+    
+    // run the program
+    // $0000: B6 11
+    // ...
+    // $0018: 53
+    // with Y = $07
+    // and expect to see $53 in X
+    
+    List<SExpression> exprs = new LinkedList<>();
+    StateVariableRegistry reg = new StateVariableRegistry();
+    
+    ROMBurner burner = new ROMBurner("TestROM");
+    burner.write(0x000, 0xB6);
+    burner.write(0x001, 0x11);
+    burner.write(0x018, 0x53);
+    // reset vector
+    burner.write(0xFFD, 0x00); // high byte
+    burner.write(0xFFC, 0x00); // low byte
+    
+    
+    // map the first and last page into RAM
+    ArrayList<PageHandler> pageHandlers = new ArrayList<>(16);
+    PageHandler ramPageHandler = burner.getCPUPageHandler();
+    PageHandler nullPageHandler = new NullPageHandler();
+    pageHandlers.add(0, ramPageHandler);
+    for (int i = 1; i < 15; ++i) {
+      pageHandlers.add(i, nullPageHandler);
+    }
+    pageHandlers.add(15, ramPageHandler);
+    
+    CodeGenerator memoryControllerFront = new CPUMemoryControllerFrontHalf(pageHandlers);
+    CodeGenerator memoryControllerBack = new CPUMemoryControllerBackHalf(pageHandlers);
+    
+    // initialize CPU once
+    CodeGenerator cpuPowerOn = new CPUPowerOn();
+    exprs.addAll(reg.apply(cpuPowerOn));
+    
+    // initialize RAM
+    CodeGenerator initRAM = burner.getInitializer();
+    exprs.addAll(reg.apply(initRAM));
+    
+    // set Y = $07
+    exprs.addAll(reg.apply(new CodeGenerator(){
+      @Override
+      public Set<String> getStateVariablesRead() {
+        return new HashSet<>();
+      }
+
+      @Override
+      public Set<String> getStateVariablesWritten() {
+        return new HashSet<String>(Arrays.asList(new String[]{
+            "CPU_Y"
+        }));
+      }
+
+      @Override
+      public List<SExpression> generateCode(Map<String, Symbol> inputs,
+          Map<String, Symbol> outputs) {
+        List<SExpression> exprs = new LinkedList<>();
+        
+        Symbol Y = outputs.get("CPU_Y");
+        exprs.add(new BitVectorDeclaration(Y, new Numeral("8")));
+        exprs.add(new Assertion(new EqualsExpression(Y, new HexConstant("07"))));
+        
+        return exprs;
+      }
+    }));
+    
+    CodeGenerator cpuCycle = new CPUCycle();
+    CodeGenerator verifyStateInstructionFetch = new VerifyCPUState(CPUState.InstructionFetch);
+    
+    // reset sequence
+    for (int i = 0; i < 8; ++i) {
+      exprs.addAll(reg.apply(cpuCycle));
+      exprs.addAll(reg.apply(memoryControllerFront));
+      exprs.addAll(reg.apply(nullPageHandler));
+      exprs.addAll(reg.apply(ramPageHandler));
+      exprs.addAll(reg.apply(memoryControllerBack));
+    }
+    exprs.addAll(reg.apply(verifyStateInstructionFetch));
+    
+    // execute instruction
+    // LDA zeropage,x takes 4 cycles
+    
+    for (int i = 0; i < 4; ++i) {
+      exprs.addAll(reg.apply(cpuCycle));
+      exprs.addAll(reg.apply(memoryControllerFront));
+      exprs.addAll(reg.apply(nullPageHandler));
+      exprs.addAll(reg.apply(ramPageHandler));
+      exprs.addAll(reg.apply(memoryControllerBack));
+    }
+    
+    // check that X = $53
+    CodeGenerator verifyX = new CodeGenerator() {
+
+      @Override
+      public Set<String> getStateVariablesRead() {
+        return new HashSet<String>(Arrays.asList(new String[]{
+            "CPU_X"
+        }));
+      }
+
+      @Override
+      public Set<String> getStateVariablesWritten() {
+        return new HashSet<>();
+      }
+
+      @Override
+      public List<SExpression> generateCode(Map<String, Symbol> inputs,
+          Map<String, Symbol> outputs) {
+        List<SExpression> exprs = new LinkedList<>();
+        
+        Symbol X = inputs.get("CPU_X");
+        exprs.add(new Assertion(new EqualsExpression(X, new HexConstant("53"))));
+        
+        return exprs;
+      }
+      
+    };
+    exprs.addAll(reg.apply(verifyX));
+    
+    
+    try(Z3 z3 = new Z3()) {
+      z3.open();
+      for(SExpression expr : exprs) {
+        z3.write(expr.toString());
+      }
+      assertTrue(z3.checkSat());
+    }
+    
+  }
+  
+  @Test
+  public void testLDX_ABS() throws IOException {
+    // opcode AE
+    
+    // run the program
+    // $0000: AE 2A 03  LDA $032A
+    // ...
+    // $032A: E2
+    // and expect to see $E2 in X
+    
+    List<SExpression> exprs = new LinkedList<>();
+    StateVariableRegistry reg = new StateVariableRegistry();
+    
+    ROMBurner burner = new ROMBurner("TestROM");
+    burner.write(0x000, 0xAE);
+    burner.write(0x001, 0x2A);
+    burner.write(0x002, 0x03);
+    burner.write(0x32A, 0xE2);
+    // reset vector
+    burner.write(0xFFD, 0x00); // high byte
+    burner.write(0xFFC, 0x00); // low byte
+    
+    
+    // map the first and last page into RAM
+    ArrayList<PageHandler> pageHandlers = new ArrayList<>(16);
+    PageHandler ramPageHandler = burner.getCPUPageHandler();
+    PageHandler nullPageHandler = new NullPageHandler();
+    pageHandlers.add(0, ramPageHandler);
+    for (int i = 1; i < 15; ++i) {
+      pageHandlers.add(i, nullPageHandler);
+    }
+    pageHandlers.add(15, ramPageHandler);
+    
+    CodeGenerator memoryControllerFront = new CPUMemoryControllerFrontHalf(pageHandlers);
+    CodeGenerator memoryControllerBack = new CPUMemoryControllerBackHalf(pageHandlers);
+    
+    // initialize CPU once
+    CodeGenerator cpuPowerOn = new CPUPowerOn();
+    exprs.addAll(reg.apply(cpuPowerOn));
+    
+    // initialize RAM
+    CodeGenerator initRAM = burner.getInitializer();
+    exprs.addAll(reg.apply(initRAM));
+    
+    CodeGenerator cpuCycle = new CPUCycle();
+    CodeGenerator verifyStateInstructionFetch = new VerifyCPUState(CPUState.InstructionFetch);
+    
+    // reset sequence
+    for (int i = 0; i < 8; ++i) {
+      exprs.addAll(reg.apply(cpuCycle));
+      exprs.addAll(reg.apply(memoryControllerFront));
+      exprs.addAll(reg.apply(nullPageHandler));
+      exprs.addAll(reg.apply(ramPageHandler));
+      exprs.addAll(reg.apply(memoryControllerBack));
+    }
+    exprs.addAll(reg.apply(verifyStateInstructionFetch));
+    
+    // execute instruction
+    // LDA absolute takes 4 cycles
+    
+    for (int i = 0; i < 4; ++i) {
+      exprs.addAll(reg.apply(cpuCycle));
+      exprs.addAll(reg.apply(memoryControllerFront));
+      exprs.addAll(reg.apply(nullPageHandler));
+      exprs.addAll(reg.apply(ramPageHandler));
+      exprs.addAll(reg.apply(memoryControllerBack));
+    }
+    
+    // check that X = $E2
+    CodeGenerator verifyX = new CodeGenerator() {
+
+      @Override
+      public Set<String> getStateVariablesRead() {
+        return new HashSet<String>(Arrays.asList(new String[]{
+            "CPU_X"
+        }));
+      }
+
+      @Override
+      public Set<String> getStateVariablesWritten() {
+        return new HashSet<>();
+      }
+
+      @Override
+      public List<SExpression> generateCode(Map<String, Symbol> inputs,
+          Map<String, Symbol> outputs) {
+        List<SExpression> exprs = new LinkedList<>();
+        
+        Symbol X = inputs.get("CPU_X");
+        exprs.add(new Assertion(new EqualsExpression(X, new HexConstant("E2"))));
+        
+        return exprs;
+      }
+      
+    };
+    exprs.addAll(reg.apply(verifyX));
+    
+    
+    try(Z3 z3 = new Z3()) {
+      z3.open();
+      for(SExpression expr : exprs) {
+        z3.write(expr.toString());
+      }
+      assertTrue(z3.checkSat());
+    }
+  }
+  @Test
+  public void testLDX_ABY() throws IOException {
+    // opcode BE
+    
+    // run the program
+    // $0000: BE 20 03 LDA $0320,Y
+    // ...
+    // $032A: 49
+    // with Y = $0A
+    // and expect to see $49 in A
+    
+    List<SExpression> exprs = new LinkedList<>();
+    StateVariableRegistry reg = new StateVariableRegistry();
+    
+    ROMBurner burner = new ROMBurner("TestROM");
+    burner.write(0x000, 0xBE);
+    burner.write(0x001, 0x20);
+    burner.write(0x002, 0x03);
+    burner.write(0x32A, 0x49);
+    // reset vector
+    burner.write(0xFFD, 0x00); // high byte
+    burner.write(0xFFC, 0x00); // low byte
+    
+    
+    // map the first and last page into RAM
+    ArrayList<PageHandler> pageHandlers = new ArrayList<>(16);
+    PageHandler ramPageHandler = burner.getCPUPageHandler();
+    PageHandler nullPageHandler = new NullPageHandler();
+    pageHandlers.add(0, ramPageHandler);
+    for (int i = 1; i < 15; ++i) {
+      pageHandlers.add(i, nullPageHandler);
+    }
+    pageHandlers.add(15, ramPageHandler);
+    
+    CodeGenerator memoryControllerFront = new CPUMemoryControllerFrontHalf(pageHandlers);
+    CodeGenerator memoryControllerBack = new CPUMemoryControllerBackHalf(pageHandlers);
+    
+    // initialize CPU once
+    CodeGenerator cpuPowerOn = new CPUPowerOn();
+    exprs.addAll(reg.apply(cpuPowerOn));
+    
+    // initialize RAM
+    CodeGenerator initRAM = burner.getInitializer();
+    exprs.addAll(reg.apply(initRAM));
+    
+    // set Y = $0A
+    exprs.addAll(reg.apply(new CodeGenerator(){
+      @Override
+      public Set<String> getStateVariablesRead() {
+        return new HashSet<>();
+      }
+
+      @Override
+      public Set<String> getStateVariablesWritten() {
+        return new HashSet<String>(Arrays.asList(new String[]{
+            "CPU_Y"
+        }));
+      }
+
+      @Override
+      public List<SExpression> generateCode(Map<String, Symbol> inputs,
+          Map<String, Symbol> outputs) {
+        List<SExpression> exprs = new LinkedList<>();
+        
+        Symbol Y = outputs.get("CPU_Y");
+        exprs.add(new BitVectorDeclaration(Y, new Numeral("8")));
+        exprs.add(new Assertion(new EqualsExpression(Y, new HexConstant("0A"))));
+        
+        return exprs;
+      }
+    }));
+    
+    CodeGenerator cpuCycle = new CPUCycle();
+    CodeGenerator verifyStateInstructionFetch = new VerifyCPUState(CPUState.InstructionFetch);
+    
+    // reset sequence
+    for (int i = 0; i < 8; ++i) {
+      exprs.addAll(reg.apply(cpuCycle));
+      exprs.addAll(reg.apply(memoryControllerFront));
+      exprs.addAll(reg.apply(nullPageHandler));
+      exprs.addAll(reg.apply(ramPageHandler));
+      exprs.addAll(reg.apply(memoryControllerBack));
+    }
+    exprs.addAll(reg.apply(verifyStateInstructionFetch));
+    
+    // execute instruction
+    // LDA absolute,y takes 4 cycles if the page is not crossed
+    
+    for (int i = 0; i < 4; ++i) {
+      exprs.addAll(reg.apply(cpuCycle));
+      exprs.addAll(reg.apply(memoryControllerFront));
+      exprs.addAll(reg.apply(nullPageHandler));
+      exprs.addAll(reg.apply(ramPageHandler));
+      exprs.addAll(reg.apply(memoryControllerBack));
+    }
+    
+    // check that X = $49
+    CodeGenerator verifyX = new CodeGenerator() {
+
+      @Override
+      public Set<String> getStateVariablesRead() {
+        return new HashSet<String>(Arrays.asList(new String[]{
+            "CPU_X"
+        }));
+      }
+
+      @Override
+      public Set<String> getStateVariablesWritten() {
+        return new HashSet<>();
+      }
+
+      @Override
+      public List<SExpression> generateCode(Map<String, Symbol> inputs,
+          Map<String, Symbol> outputs) {
+        List<SExpression> exprs = new LinkedList<>();
+        
+        Symbol X = inputs.get("CPU_X");
+        exprs.add(new Assertion(new EqualsExpression(X, new HexConstant("49"))));
+        
+        return exprs;
+      }
+      
+    };
+    exprs.addAll(reg.apply(verifyX));
+    
+    
+    try(Z3 z3 = new Z3()) {
+      z3.open();
+      for(SExpression expr : exprs) {
+        z3.write(expr.toString());
+      }
+      assertTrue(z3.checkSat());
+    }
+  }
+  
+}
